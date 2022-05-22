56ff8dc6d3510bc6b70ae552ffbb2510611f68df
keremdokumaci <kerem.dokumaci@yemeksepeti.com>
2022-05-22 18:54:08 +0300

    add: proxy for tureng.com

--- app/proxy.go
+++ app/proxy.go
@@ -1,9 +1,39 @@
 package app
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
 type TurengProxy struct {
-	Language string
+	client   *http.Client
+	language string
+}
+
+func NewTurengProxy(language string) *TurengProxy {
+	return &TurengProxy{
+		language: language,
+		client: &http.Client{
+			Timeout: time.Second * 5,
+		},
+	}
 }
 
 func (p *TurengProxy) Query(word string) (string, error) {
-	return "", nil
+	url := fmt.Sprintf("https://tureng.com/en/%s/%s", p.language, word)
+
+	resp, err := p.client.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }

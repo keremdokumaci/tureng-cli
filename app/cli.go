87aed425b72b6d453a9e262a661a02dec18d657e
keremdokumaci <kerem.dokumaci@yemeksepeti.com>
2022-05-22 18:54:02 +0300

    add: cli command and query seperation

--- app/cli.go
+++ app/cli.go
@@ -2,8 +2,11 @@ package app
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 type Language string
@@ -18,17 +21,19 @@ const (
 type Command string
 
 const (
-	DEFAULT         Command = "search"
 	UPDATE_LANGUAGE Command = "update-language"
+	CLEAR           Command = "clear"
 )
 
 type TurengCli struct {
+	proxy    *TurengProxy
 	language Language `validate:"required"`
 }
 
 func NewCli(language Language) *TurengCli {
 	cli := &TurengCli{}
 	cli.language = language
+	cli.proxy = NewTurengProxy(string(language))
 	return cli
 }
 
@@ -36,7 +41,20 @@ func (c *TurengCli) Run() {
 	for {
 		c.showPrompt()
 		input := c.getUserInput()
-		fmt.Println(input)
+
+		if c.isCommandText(input) {
+			err := c.runCommand(input)
+			if err != nil && err.Error() != fmt.Sprintf(`exec: "%s": executable file not found in $PATH`, input) {
+				fmt.Println(err.Error())
+				continue
+			}
+		} else {
+			result, err := c.proxy.Query(input)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Println(result)
+		}
 	}
 }
 
@@ -50,4 +68,44 @@ func (c *TurengCli) getUserInput() string {
 
 	inputText := input.Text()
 	return inputText
+}
+
+func (c *TurengCli) isCommandText(text string) bool {
+	return strings.Split(text, "")[0] == "["
+}
+
+func (c *TurengCli) runCommand(text string) error {
+	text = text[1:]
+	text = text[:len(text)-1]
+
+	commands := strings.Split(text, " ")
+	cmd := commands[0]
+
+	switch cmd {
+	case string(UPDATE_LANGUAGE):
+		return c.updateLanguage(commands[1])
+	default:
+		c := exec.Command(text)
+		c.Stdout = os.Stdout
+		err := c.Run()
+		return err
+	}
+}
+
+func (c *TurengCli) updateLanguage(language string) error {
+	switch language {
+	case string(TREN):
+		break
+	case string(ENDE):
+		break
+	case string(ENES):
+		break
+	case string(ENFR):
+		break
+	default:
+		return errors.New("unsupported language")
+	}
+
+	c.language = Language(language)
+	return nil
 }
